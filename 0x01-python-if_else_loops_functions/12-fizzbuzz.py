516960b23c750c1a10006776770af8274f4704ba
paotec22 <paotec22@gmial.com>
2022-09-25 16:08:15 -0700

    fuzz & Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,6 +5,6 @@ def fizzbuzz():
             print('Fizz', end='')
         if i % 5 == 0:
             print('Buzz', end='')
-        if i % 3 and i % 5 == 0:
-            print("{}".format(i), end='')
+        if i % 3 and i % 5:
+            print("{:d}".format(i), end='')
         print(end=' ')
