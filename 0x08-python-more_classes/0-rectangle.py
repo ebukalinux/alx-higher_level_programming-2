b575b4aafff9ae623ed151137cad2587ac148ed4
paotec22 <paotec22@gmial.com>
2022-10-19 23:14:43 -0700

    empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    Empty Class that defines a rectangle
+    """
+    pass#!/usr/bin/python3
+class Rectangle:
+    """
+    Empty Class that defines a rectangle
+    """
+    pass
